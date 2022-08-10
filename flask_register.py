072aa7afb28190c1d981c5640aa6cab40ca23628
Your Name <you@example.com>
2022-08-10 22:42:06 +0900

    make it better

--- flask_register.py
+++ flask_register.py
@@ -13,12 +13,12 @@ app = Flask(__name__)
 #  新版本的部份預設為none，會有異常，再設置True即可。
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #  設置資料庫為sqlite3
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9'
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9']\
  #  or 'sqlite:///' + os.path.join(pjdir,'data.sqlite')
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9']\
-  # = 'postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9']\
+   = 'postgresql://yuacqtdojxgkqv:28050498cf7a8c7f569598e1b9adc03b80349dbb3e0b0d91ab328aa0156524b8@ec2-3-225-110-188.compute-1.amazonaws.com:5432/d8q79pjsluumf9'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
                                        #os.path.join(pjdir, 'data_register.sqlite')
 
@@ -37,7 +37,7 @@ with app.app_context():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     from form import FormRegister
-    from model import UserReister
+    from model import UserReister,db
     form =FormRegister()
     if form.validate_on_submit():
         user = UserReister(
