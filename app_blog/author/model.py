ecd43a2650e8ee13586a048cbe3b613b8c49862d
Your Name <you@example.com>
2022-08-22 15:44:52 +0900

    make it better

--- app_blog/author/model.py
+++ app_blog/author/model.py
@@ -9,7 +9,7 @@ class UserRegister(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(50), nullable=False) 
+    password_hash = db.Column(db.String(100), nullable=False) 
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
 
